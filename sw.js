a8eebfb50bc8b33f23b80ea0b90b1f3bf8099b2c
dani85-art <daniel.estrella.silgado@gmail.com>
2026-02-10 21:21:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'produccion-v2';
+const CACHE_NAME = 'produccion-v3';
 
 const FILES_TO_CACHE = [
   './',
@@ -32,4 +32,5 @@ self.addEventListener('fetch', e => {
     caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
+
 
